7ccd8539e898d1f9abac16fac818dcfcde1bceb7
Maguette Gueye Mbow <maguettegueyembow6@gmail.com>
2026-06-13 20:55:17 +0000

    JS : dark mode avec localStorage, navbar dynamique au scroll

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const toggleBtn = document.getElementById("theme-toggle");
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark-mode");
+}
+toggleBtn.addEventListener ("click" , () => {
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        localStorage.setItem("theme","dark");
+    }
+    else{
+        localStorage.setItem("theme","light");
+    }
+});
+window.addEventListener("scroll", () =>{
+    const navbar = document.querySelector(".navbar");
+    if(window.scrollY > 50){
+        navbar.classList.remove("scrolled");
+    }
+});
+const backToTop = document.getElementById("backToTop");
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY > 300){
+        backToTop.style.display = "block";
+    }
+    else{
+        backToTop.style.display = "none";
+    }
+});
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
