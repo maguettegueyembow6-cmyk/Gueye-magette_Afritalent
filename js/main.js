fc159f53c434dbbada1a34bec434b903d938c5a7
Maguette Gueye Mbow <maguettegueyembow6@gmail.com>
2026-06-27 21:58:45 +0000

    JS : filtrage dynamique des freelances et validation du formulaire de contact

--- js/main.js
+++ js/main.js
@@ -68,3 +68,79 @@ const fadeObserver = new IntersectionObserver((entries) => {
 });
 sections.forEach(section => fadeObserver.observe(section));
 
+//filtrage des freelances
+const filterBouttons = document.querySelectorAll(".filter-btn");
+const freelances = document.querySelectorAll(".freelances");
+
+filterBouttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const filter = button.dataset.filter;
+
+        freelances.forEach(card => {
+            if (filter === "all" || card.dataset.category === filter) {
+                card.style.display = "block";
+            }
+            else {
+                card.style.display = "none";
+            }
+        });
+    });
+});
+
+//Validation du formulaire 
+const form = document.getElementById("contactForm");
+if (form) {
+    form.addEventListener("submit", function (e){
+        e.preventDefault();
+        const nom = document.getElementById("nom");
+        const prenom = document.getElementById("prenom");
+        const email = document.getElementById("email");
+        const sujet = document.getElementById("sujet");
+        const message = document.getElementById("message");
+        let valide = true;
+        function erreur(champ, texte) {
+            champ.nextElementSibling.textContent = texte;
+            valide = false ;
+        }
+        function succes(champ) {
+            champ.nextElementSibling.textContent = "";
+        }
+        //nom
+        if (nom.value.trim() === ""){
+            erreur(nom, "Le nom est obligatoire");
+        }   else {
+            succces(nom)
+        }
+        //prenom
+        if (prenom.value.trim() === ""){
+            erreur(nom, "Le prenom est obligatoire");
+        }   else {
+            succces(nom)
+        }
+        //email
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!regex.text(email.values)) {
+            erreur(email, "Email invalide");
+        }   else {
+            succes(email);
+        }
+        //sujet
+        if (sujet.values === "") {
+            erreur(sujet, "Choisissez un sujet");
+        }   else {
+            succes(sujet);
+        }
+        //message
+        if (message.value.trim().length < 20) {
+            erreur(message, "Le message contenir au moins 20 caractéres");
+        }   else {
+            succes(message);
+        }
+        if (valide) {
+            alert("Message envoyé avec succés !");
+            form.reset();
+        }
+    });
+}
+
